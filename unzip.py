f39774b2bb3c8231372c75f0aa6df7837f7b0630
axeloh <axel.harstad@gmail.com>
2022-10-09 04:07:06 +0200

    initial

---
+++ unzip.py
@@ -0,0 +1,19 @@
+"""Module to extract all pgns from zip files."""
+
+from tqdm import tqdm
+import zipfile
+import os
+
+DATA_FOLDER = 'data'
+OUT_FOLDER = 'data'
+
+def unzip(path: str, delete_zip: bool = False):
+    with zipfile.ZipFile(path, 'r') as zip_ref:
+        zip_ref.extractall(OUT_FOLDER)
+    if delete_zip:
+        os.remove(path) 
+
+for file in tqdm(os.listdir(DATA_FOLDER)):
+    if not file.endswith('.zip'):
+        continue
+    unzip(f'{DATA_FOLDER}/{file}', delete_zip=True)
